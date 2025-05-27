d24e624d13f44d5b8c14bd2f1e9e32d7f9414f4e
Marco Madini Moretti <marco.madinimoretti@trebi.it>
2025-05-28 01:45:58 +0200

    configurazione deploy bis

--- escape-room-virtual/vite.config.js
+++ escape-room-virtual/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const BASE_PATH = '/escape-room-virtual/'; 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+	plugins: [
 		react(),
 		tailwindcss(),
 	],
+	base: BASE_PATH,
 })
